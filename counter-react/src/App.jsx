fba81c0a6411cab2641c261ed6c2d83cd978ddf1
Yash Chandanshive <yashchandanshive18@gmail.com>
2025-03-10 17:41:48 +0530

    completed count project

--- counter-react/src/App.jsx
+++ counter-react/src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [count, setCount] =useState(5)
+
+  function increment(){
+    if(count===20) { 
+      alert('Count cannot be set above 20') 
+      return}
+    setCount((prev)=>prev+1)
+ 
+  }
+
+  function decrement(){
+    if(count==0) {
+      alert('Count cannot be set below 0')
+      return
+    }
+    setCount((prev)=>prev-1)
+  }
+  function reset(){
+    setCount(5)
+    alert('Count reset to 5')
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <h1 className="font-sans font-semibold">Count : {count}</h1>
+    <button className="mr-2 mt-5"  onClick={increment}>Increment</button>
+    <button className="mr-2" onClick={decrement}>Decrement</button>
+    <button onClick={reset}>Reset</button>
+
+</>
   )
 }
 
